58b6a64e815d5987f6fc1f314fcc233b412a0aed
Christina Holland <hsubox@gmail.com>
2018-07-14 00:40:52 +0000

    Add some more tasks

--- src/data/tasks.js
+++ src/data/tasks.js
@@ -20,36 +20,90 @@
 
 export const INITIAL_TASK_IDS = [1, 2, 3];
 
+const DAY = 24;
+
 export const TASKS = [
   {
     taskId: 1,
     description: 'Water plant',
     spawnsOnDone: [
-      { taskId: 1, delay: 24 },
+      { taskId: 1, delay: 1 * DAY },
     ],
     spawnsOnAge: [
-      { age: 72, taskId: 4, killParent: true }
+      { age: 3 * DAY, taskId: 4, killParent: true }
     ]
     
   },
-  { taskId: 2, description: 'Call mom long name test long wrapping text name' },
-  { taskId: 3, description: 'Text K' },
+  { taskId: 2, description: 'Call mom' },
+  {
+    taskId: 3,
+    description: 'Text K about how job interview went',
+    spawnsOnDone: [
+      { taskId: 7, delay: 1 * DAY },
+    ],
+    spawnsOnAge: [
+      { age: 2 * DAY, taskId: 8, killParent: true }
+    ]
+  },
   {
     taskId: 4,
     description: 'Take dead plant out to garbage',
     spawnsOnAge: [
-      { age: 72, taskId: 5, killParent: true }
+      { age: 3 * DAY, taskId: 5, killParent: true }
     ]
   },
   {
     taskId: 5,
     description: 'Buy flypaper to catch flies around rotting plant',
     spawnsOnAge: [
-      { age: 72, taskId: 6, killParent: true }
+      { age: 3 * DAY, taskId: 6, killParent: true }
     ]
   },
   {
     taskId: 6,
     description: 'Find out where maggots are coming from'
-  }
+  },
+  {
+    taskId: 7,
+    description: 'Read that book K was talking about (Atlas something?)',
+    spawnsOnDone: [
+      { taskId: 12, delay: 1 * DAY },
+    ],
+  },
+  {
+    taskId: 8,
+    description: 'Come up with plausible excuse for not asking K about job interview',
+    spawnsOnAge: [
+      { age: 2 * DAY, taskId: 9, killParent: true }
+    ]
+  },
+  {
+    taskId: 9,
+    description: 'Apologize to K',
+    spawnsOnAge: [
+      { age: 2 * DAY, taskId: 10, killParent: true }
+    ]
+  },
+  {
+    taskId: 10,
+    description: 'Plan grand gesture to make everything up to K',
+    spawnsOnAge: [
+      { age: 2 * DAY, taskId: 11, killParent: true }
+    ]
+  },
+  {
+    taskId: 11,
+    description: 'Change Facebook status to "not in a relationship"'
+  },
+  {
+    taskId: 12,
+    description: 'Write a diplomatic, thoughtful email to K about how bad Atlas Shrugged was (NOTHING PERSONAL)',
+    spawnsOnDone: [
+      { taskId: 13, delay: 1 * DAY },
+    ],
+  },
+  {
+    taskId: 13,
+    description: 'Ask L how to set up Gmail filter to block angry messages from K'
+  },
 ];
